5b571dbbddb3f2bbe09deefd06048d83849498ea
randy-orton <reddy.sivareddy2@gmail.com>
2020-11-07 12:33:17 +0530

    implemented validator duplicate email ids

--- OrderItemService1/src/main/java/com/orderitem/demo/controller/Custmoersave_data_saving_controller.java
+++ OrderItemService1/src/main/java/com/orderitem/demo/controller/Custmoersave_data_saving_controller.java
@@ -3,14 +3,16 @@ package com.orderitem.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.orderitem.demo.model.CustmoerData;
-import com.orderitem.demo.model.OrderServiceHistory;
 import com.orderitem.demo.repo.CustmerSaveDataRepo;
+import com.orderitem.demo.validator.Custmerdata_validation;
 
 @RestController
 public class Custmoersave_data_saving_controller {
@@ -18,7 +20,19 @@ public class Custmoersave_data_saving_controller {
 	@Autowired
 	private CustmerSaveDataRepo custmersave;
 
-	 
+	@Autowired
+	private Custmerdata_validation prv; 
+	
+
+	@InitBinder
+	public void init(WebDataBinder web)
+	{
+		 
+		  web.addValidators(prv);
+		
+		
+	}
+	
 
 	@RequestMapping(method = RequestMethod.POST, value = "/savecustomer")
 	public ResponseEntity<CustmoerData> postorderservice(@RequestBody CustmoerData data1) {
--- OrderItemService1/src/main/java/com/orderitem/demo/repo/CustmerSaveDataRepo.java
+++ OrderItemService1/src/main/java/com/orderitem/demo/repo/CustmerSaveDataRepo.java
@@ -1,5 +1,7 @@
 package com.orderitem.demo.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,10 +13,18 @@ import com.orderitem.demo.model.CustmoerData;
 
 import com.orderitem.demo.model.OrderItem;
 @Repository
-public interface CustmerSaveDataRepo extends JpaRepository<OrderItem,Integer> {
+public interface CustmerSaveDataRepo extends JpaRepository<CustmoerData,Integer> {
 	
-	 
+	
 	public CustmoerData  save(CustmoerData entity);
+	public Optional<CustmoerData> findByCust_emailadress(String emailadress);
+	/*public Optional<OrderItem> findByProductName(String productname);
+
+	 
+	 private String custname;
+	 private String cust_emailadress;
+	 private String cust_pan;
+	 */
 	 
 	 
 
